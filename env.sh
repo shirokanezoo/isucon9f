0860beab6ab2b8259d351503bc5163f0169c8474
Sorah Fukumori <sorah@cookpad.com>
2019-10-05 10:57:05 +0900

    isu3 is now running mysql

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 RACK_ENV=production
 PAYMENT_API=https://payment041.isucon9.hinatan.net
 
-#MYSQL_HOSTNAME=isu3
-#MYSQL_PORT=3306
-#MYSQL_USER=isutrain
-#MYSQL_DATABASE=isutrain
-#MYSQL_PASSWORD=isutrain
+MYSQL_HOSTNAME=isu3
+MYSQL_PORT=3306
+MYSQL_USER=isutrain
+MYSQL_DATABASE=isutrain
+MYSQL_PASSWORD=isutrain
 #REDIS_URL=redis://isu3:6379/0
 NEW_RELIC_AGENT_ENABLED=true
 
