2787cd6f062dcb540edb1531dfcb75acc69f97f7
Issei Naruta <naruta@cookpad.com>
2019-10-05 13:54:51 +0900

    Truncate /tmp/isu-params.log after deploy

--- deploy.sh
+++ deploy.sh
@@ -31,4 +31,4 @@ sudo nginx -s reload || :
   bundle exec newrelic deployment -r "$(git rev-parse HEAD)"
 ) || :
 
-sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s) && echo > /var/log/nginx/access.log; echo > /tmp/isu-query.log; echo > /tmp/isu-rack.log; test -d /tmp/stackprof && rm -f /tmp/stackprof/*; echo > /var/lib/mysql/mysql-slow.log; chown isucon:isucon /tmp/isu*.log'
+sudo bash -c 'cp /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%s) && echo > /var/log/nginx/access.log; echo > /tmp/isu-query.log; echo > /tmp/isu-rack.log; echo > /tmp/isu-params.log; test -d /tmp/stackprof && rm -f /tmp/stackprof/*; echo > /var/lib/mysql/mysql-slow.log; chown isucon:isucon /tmp/isu*.log'
